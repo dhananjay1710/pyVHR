a7012bcd219dcfac35d14822d3786f98573faf2b
Dhananjay <42811038+dhananjay1710@users.noreply.github.com>
2021-07-05 16:03:15 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-PYTHONPATH=/home/site/wwwroot/venv/lib/python3.7/site-packages:${PYTHONPATH}
-export PYTHONPATH
-apt update
-apt-get install libgfortran3 -y
-apt-get install gfortran -y
-pip install numpy
-pip install pybdf
-gunicorn --bind=0.0.0.0 app:app
